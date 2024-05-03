ac8aa54120f31e556f40bae2c3f4a124b84dfefb
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-03 14:07:39 +0200

    added IDisposable implementation

--- ORM/DataContext/DataContext.cs
+++ ORM/DataContext/DataContext.cs
@@ -1,25 +1,40 @@
 ﻿using Microsoft.Data.SqlClient;
 using ORM.BaseClass;
+using ORM.Manager;
 using ORM.Schema;
 
 namespace ORM.DataContextImplementation
 {
 
-    public class DataContext : IDisposable
+    public class DataContext<T> : IDisposable where T : IEntity
     {
+        private bool isDisposed;
+
+        private ITableIO<T> _tableIO;
+
         public SqlConnection Connection {  get; set; }
 
         public DataContext(string connection)
         {
             Connection = new SqlConnection(connection);
-            Connection.Open();
         }
 
-      
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!isDisposed) 
+            { 
+                if(disposing)
+                {
+                    Connection.Dispose();
+                }
+                isDisposed = true;
+            }
+        }
     }
 }
