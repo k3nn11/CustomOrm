43a33b7447f2af31612ac6b4869e2fe142e8611b
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-06 18:57:06 +0200

    The Interface and the class created are resposnsible for interacting with the database. Read and Write operations.

---
+++ ORM/Context/DBContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+using ORM.BaseClass;
+using System;
+
+
+namespace ORM.Context
+{
+    public class DBContext<T> : IDisposable where T : IEntity
+    {
+        private bool _IsDisposed;
+
+        public DBContext(IDbSet<T> _DbSet, string connection)
+        {
+            DbSet = _DbSet;
+            Connection = new SqlConnection(connection);
+        }
+
+        public IDbSet<T> DbSet { get; private set; }
+
+        public SqlConnection Connection { get; private set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_IsDisposed)
+            {
+                if (disposing)
+                {
+                    Connection.Dispose();
+                }
+                _IsDisposed = true;
+            }
+        }
+    }
+}
---
+++ ORM/Context/IDbSet.cs
@@ -0,0 +1,28 @@
+﻿using ORM.BaseClass;
+using System.Linq.Expressions;
+
+namespace ORM.Context
+{
+    public interface IDbSet<T> where T : IEntity
+    {
+        public IEnumerable<T> Select();
+
+        public IEnumerable<T> Select(Expression<Func<T, bool>> expression);
+
+        public void Insert(T entity);
+
+        public void Insert(IEnumerable<T> entities);
+
+        public void Update(T entity);
+
+        public void Update(IEnumerable<T> entities);
+
+        public int Delete(T entity);
+
+        public int Delete(IEnumerable<T> entities);
+
+        public int Delete(Expression<Func<T, bool>> expression);
+
+        public int DeleteAll();
+    }
+}
