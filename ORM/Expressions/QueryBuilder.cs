464d968872c69d21e363c98488ed1e630db1faef
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-06 18:57:32 +0200

    Added class to translate expressions

---
+++ ORM/Expressions/QueryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using System.Text;
+
+namespace ORM.Expressions
+{
+    public class QueryBuilder : ExpressionVisitor
+    {
+
+        private StringBuilder? _sb;
+
+        public string? WhereClause { get; private set; }
+
+        public string Translate(Expression expression)
+        {
+           _sb = new StringBuilder();
+            this.Visit(expression);
+            WhereClause = _sb.ToString();
+            return WhereClause;
+        }
+    }
+}
