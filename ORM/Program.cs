368ddab029fd6ca3b511838c355f691e5850158a
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-07 22:43:59 +0200

    Changed to correct types of columns

--- ORM/Program.cs
+++ ORM/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using ORM.ConnectionFactory;
+using ORM.Context;
+using ORM.Repository;
+using ORM.Schema;
+using System.Reflection;
 
 namespace ORM
 {
@@ -6,7 +10,20 @@ namespace ORM
     {
         static void Main(string[] args)
         {
-            
+            User person = new User();
+            person.Id = 1;
+            person.Description = "Description";
+            person.Rating = 1;
+            person.Name = "James";
+
+            string connection = "Data Source=(localdb)\\ProjectModels;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            IProvider provider = new SqlProvider(connection);
+            DataBaseManager dbmanager = new("CustomOrm", provider, assembly);
+            IDbSet<User> set = (IDbSet<User>)dbmanager.GetTable(typeof(User));
+            DBContext<User> context = new(set);
+            Repository<User> repository = new(context);
+            repository.Add(person);
         }
     }
 }
