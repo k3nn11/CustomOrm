db7be5ef47997f15af86845fd87947cb76ac6f92
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-20 15:54:51 +0200

    Added HasAttribute method

--- ORM/Extension/TypeExtension.cs
+++ ORM/Extension/TypeExtension.cs
@@ -1,10 +1,17 @@
-﻿namespace ORM.Extension
+﻿using System.Reflection;
+
+namespace ORM.Extension
 {
     public static class TypeExtension
     {
         public static bool HasBaseClass<T>(this Type type) where T : class
         {
             return type.GetInterfaces().Any(x => x == typeof(T));
+        }
+
+        public static bool HasAttribute<T>(this PropertyInfo property)
+        {
+            return property.CustomAttributes.Any(x => x.AttributeType == typeof(T));
         }
     }
 }
