d1538d6d73f45954e28506f45ef1ac3ebfa8b641
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-21 15:52:46 +0200

    Added properties

--- ORM/CustomAttribute/AutoIncrement.cs
+++ ORM/CustomAttribute/AutoIncrement.cs
@@ -1,9 +1,15 @@
 ﻿
 namespace ORM.CustomAttribute
 {
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
     public class AutoIncrement : Attribute
     {
+        public int AutoIncrementSeed {  get; set; }
+        public int AutoIncrementStep { get; set; }
         public AutoIncrement() 
-        { }
+        {
+            AutoIncrementSeed = 1;
+            AutoIncrementStep = 1;
+        }
     }
 }
