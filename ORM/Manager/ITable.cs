d3764d94ddc7a2d2d506b02ceffb1cf9cbb75633
Kenendy.Githua <githuanjuguna@gmail.com>
2024-05-21 12:00:00 +0200

    Added UpdateTable method interface.

--- ORM/Manager/ITable.cs
+++ ORM/Manager/ITable.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ORM.Manager
+﻿namespace ORM.Manager
 {
     public interface ITable
     {
         string Name { get; }
 
         void CreateTable();
+
+        void UpdateTable();
 
     }
 }
